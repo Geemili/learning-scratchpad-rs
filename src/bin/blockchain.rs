d09f1c5ed83bbb5f41380b59faeac30d88814126
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-04-05 21:17:23 -0500

    Add blocks to blockchain

--- src/bin/blockchain.rs
+++ src/bin/blockchain.rs
@@ -1,41 +1,148 @@
 extern crate sha2;
-#[macro_use] extern crate quicli;
 extern crate rand;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate rmp_serde;
 
 use sha2::{Sha256, Digest};
-use quicli::prelude::*;
 use rand::{Rng, thread_rng};
-use std::collections::HashMap;
-
-#[derive(Debug, StructOpt)]
-struct Cli {
-}
+use std::collections::BTreeMap;
+use serde::Serialize;
+use rmp_serde::Serializer;
 
 const ALICE_ID: AccountId = 1;
 const BOB_ID: AccountId = 2;
 const LISA_ID: AccountId = 3;
 
-main!(|_args: Cli| {
-    let mut balances = HashMap::new();
-    balances.insert(ALICE_ID, 5);
-    balances.insert(BOB_ID, 5);
-    let state = State { balances };
+// Default limit on random transactions
+const MAX_AMOUNT: i64 = 3;
+const BLOCK_SIZE_LIMIT: usize = 5;
 
-    println!("{}", Transaction::from_vec(&vec![(ALICE_ID, -3), (BOB_ID, 3)]).is_valid(&state));
-    println!("{}", Transaction::from_vec(&vec![(ALICE_ID, -4), (BOB_ID, 3)]).is_valid(&state));
-    println!("{}", Transaction::from_vec(&vec![(ALICE_ID, -6), (BOB_ID, 6)]).is_valid(&state));
-    println!("{}", Transaction::from_vec(&vec![(ALICE_ID, -4), (BOB_ID, 2), (LISA_ID, 2)]).is_valid(&state));
-    println!("{}", Transaction::from_vec(&vec![(ALICE_ID, -4), (BOB_ID, 3), (LISA_ID, 2)]).is_valid(&state));
-});
+fn main() {
+    let balances = vec![
+        (ALICE_ID, 50),
+        (BOB_ID,   50),
+    ].iter().cloned().collect();
+    let mut state = State { balances };
+
+    let genesis_contents = BlockContents {
+        block_number: 0,
+        parent_hash: None,
+        transactions: vec![Transaction::from_btree(&state.balances)],
+    };
+    let genesis_block = Block::new(genesis_contents);
+
+    let mut chain = vec![ genesis_block ];
+
+    let mut txns = (0..30).map(|_| make_transaction(MAX_AMOUNT));
+
+    'LOOP:
+    loop {
+        let mut txn_list = vec![];
+        while txn_list.len() < BLOCK_SIZE_LIMIT {
+            let txn = if let Some(t) = txns.next() { t } else { break 'LOOP };
+            if txn.is_valid(&state) {
+                state = state.update(&txn);
+                txn_list.push(txn);
+            } else {
+                println!("ignored transaction");
+                continue;
+            }
+        }
+
+        let block = make_block(&txn_list, &chain);
+        chain.push(block);
+    }
+
+    println!("Chain: {:?}", chain);
+    for block in chain.iter() {
+        println!("Block Number: {}", block.contents.block_number);
+
+        print!("Hash: ");
+        for val in block.hash.iter() {
+            print!("{:x}", val);
+        }
+        println!("");
+
+        if let Some(ref parent_hash) = block.contents.parent_hash {
+            print!("Parent Hash: ");
+            for val in parent_hash.iter() {
+                print!("{:x}", val);
+            }
+            println!("");
+        }
+
+        for txn in block.contents.transactions.iter() {
+            println!("  txn:");
+            for (account, amount) in txn.amounts.iter() {
+                println!("    {:04x}: {}", account, amount);
+            }
+        }
+
+        println!("\n---\n");
+    }
+
+    println!("Final State:");
+    for (account, bal) in state.balances.iter() {
+        let name = match *account {
+            ALICE_ID => "Alice".into(),
+            BOB_ID => "Bob".into(),
+            n => format!("0x{:04x}", n),
+        };
+        println!("  {}: {}", name, bal);
+    }
+}
+
+fn make_block(transactions: &[Transaction], chain: &[Block]) -> Block {
+    let parent = chain.last().unwrap();
+    let block_number = parent.contents.block_number + 1;
+    let contents = BlockContents {
+        block_number,
+        parent_hash: Some(parent.hash.clone()),
+        transactions: transactions.iter().cloned().collect(),
+    };
+    Block::new(contents)
+}
+
+#[derive(Serialize, Clone, Debug)]
+struct Block {
+    pub hash: Vec<u8>,
+    pub contents: BlockContents,
+}
+
+impl Block {
+    pub fn new(contents: BlockContents) -> Self {
+        let hash = contents.hash();
+        Self { hash, contents }
+    }
+}
+
+#[derive(Serialize, Clone, Debug)]
+struct BlockContents {
+    pub block_number: u64,
+    pub parent_hash: Option<Vec<u8>>,
+    pub transactions: Vec<Transaction>,
+}
+
+impl BlockContents {
+    fn hash(&self) -> Vec<u8> {
+        let mut buf = vec![];
+        self.serialize(&mut rmp_serde::Serializer::new(&mut buf)).unwrap();
+        let mut hasher = Sha256::default();
+        hasher.input(&buf);
+        hasher.result().iter().cloned().collect()
+    }
+}
 
 type AccountId = u64;
 
+#[derive(Serialize, Clone, Debug)]
 struct State {
-    pub balances: HashMap<AccountId, i64>,
+    pub balances: BTreeMap<AccountId, i64>,
 }
 
 impl State {
-    fn update(&self, txn: Transaction) -> Self {
+    fn update(&self, txn: &Transaction) -> Self {
         let mut balances = self.balances.clone();
         for (key, val) in txn.amounts.iter() {
             let balance = balances.entry(*key).or_insert(0);
@@ -46,14 +153,19 @@ impl State {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Serialize, Clone, Debug)]
 struct Transaction {
-    pub amounts: HashMap<AccountId, i64>,
+    pub amounts: BTreeMap<AccountId, i64>,
 }
 
 impl Transaction {
     pub fn from_vec(amounts: &Vec<(AccountId, i64)>) -> Self {
-        let amounts: HashMap<AccountId, i64> = amounts.iter().cloned().collect();
+        let amounts: BTreeMap<AccountId, i64> = amounts.iter().cloned().collect();
+        Self { amounts }
+    }
+
+    pub fn from_btree(amounts: &BTreeMap<AccountId, i64>) -> Self {
+        let amounts = amounts.clone();
         Self { amounts }
     }
 
@@ -76,10 +188,10 @@ fn make_transaction(max_value: i64) -> Transaction {
     assert!(max_value > 0);
     let mut rng = thread_rng();
 
-    let amount = rng.gen_range(0, max_value);
+    let amount = rng.gen_range(1, max_value);
     let amount = if rng.gen() { -amount } else { amount };
 
-    let mut amounts = HashMap::new();
+    let mut amounts = BTreeMap::new();
     amounts.insert(ALICE_ID, amount);
     amounts.insert(BOB_ID, -amount);
 
